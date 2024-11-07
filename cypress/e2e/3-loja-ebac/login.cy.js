aefe91f814208bacdbe6f35c59805e3a68d94485
Deivid Paio <deividjpaio@gmail.com>
2024-11-07 10:57:04 -0300

    alteracao no espacamento

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -7,6 +7,7 @@ describe('Funcionalidade: login', () => {
         cy.get('#username').type('deividpaio_teste@gmail.com')
         cy.get('#password').type('djp001')
         cy.get('.woocommerce-form > .button').click()
+        
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, deividpaio_teste (não é deividpaio_teste? Sair)')
     })
 
